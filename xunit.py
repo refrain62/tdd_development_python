4c5448d186eb25c4d224f6f3ba1f1fba5487533b
nao <nao_h13@hotmail.com>
2022-04-08 00:11:56 +0900

    ログの内容が変わったので対応

--- xunit.py
+++ xunit.py
@@ -41,7 +41,7 @@ class TestCaseTest( TestCase ):
 
     def testSetUp( self ):
         self.test.run()
-        assert( "setUp " == self.test.log )
+        assert( "setUp testMethod " == self.test.log )
 
 
 TestCaseTest("testRunning").run()

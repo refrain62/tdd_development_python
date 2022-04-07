a06173ecf8f19fbf3627176749e3451bd00203a0
nao <nao_h13@hotmail.com>
2022-04-08 00:04:22 +0900

    SetUp を最初に呼び出す

--- xunit.py
+++ xunit.py
@@ -1,7 +1,7 @@
 
 # TODO LIST
 # ◎テストメソッドを呼び出す
-# ・SetUp を最初に呼び出す
+# ◎SetUp を最初に呼び出す
 # ・tearDownを後で呼び出す
 # ・テストメソッドが失敗したとしても tearDown を呼び出す
 # ・複数のテストを走らせる
@@ -38,15 +38,16 @@ class WasRun( TestCase ):
 
 
 class TestCaseTest( TestCase ):
+    def setUp( self ):
+        self.test = WasRun( "testMethod" )
+
     def testRunning( self ):
-        test = WasRun("testMethod")
-        test.run()
-        assert( test.wasRun )
+        self.test.run()
+        assert( self.test.wasRun )
 
     def testSetUp( self ):
-        test = WasRun( "testMethod" )
-        test.run()
-        assert( test.wasSetUp )
+        self.test.run()
+        assert( self.test.wasSetUp )
 
 
 TestCaseTest("testRunning").run()

ac96e8ecd7f920e3131571ac353d61343e3d1457
nao <nao_h13@hotmail.com>
2022-04-07 23:39:28 +0900

    name属性を親クラスに引き上げ

--- xunit.py
+++ xunit.py
@@ -9,13 +9,14 @@
 # ・
 
 class TestCase:
-    pass
+    def __init__( self, name ):
+        self.name = name
 
 
 class WasRun( TestCase ):
     def __init__( self, name):
         self.wasRun = None
-        self.name = name
+        super().__init__( name )
     
     def run( self ):
         method = getattr( self, self.name )

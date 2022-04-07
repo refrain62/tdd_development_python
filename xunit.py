277054340e2f90082a92b6f238b580f4f4770eb5
nao <nao_h13@hotmail.com>
2022-04-08 00:26:28 +0900

    tearDown の実装 したがエラー

--- xunit.py
+++ xunit.py
@@ -19,6 +19,7 @@ class TestCase:
         self.setUp()
         method = getattr( self, self.name )
         method()
+        self.tearDown()
 
 
 class WasRun( TestCase ):
@@ -27,6 +28,9 @@ class WasRun( TestCase ):
     
     def testMethod( self ):
         self.log = self.log + "testMethod "
+
+    def tearDown( self ):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest( TestCase ):

b1f7c19b3bcf1f54d962b127b756ef80e5037f48
nao <nao_h13@hotmail.com>
2022-04-07 22:58:11 +0900

    まずはテストの実装

---
+++ xunit.py
@@ -0,0 +1,17 @@
+
+# TODO LIST
+# ・テストメソッドを呼び出す
+# ・SetUp を最初に呼び出す
+# ・tearDownを後で呼び出す
+# ・テストメソッドが失敗したとしても tearDown を呼び出す
+# ・複数のテストを走らせる
+# ・収集したテスト結果を出力する
+# ・
+
+test = WasRun( "testMethod" )
+
+print( test.wasRun )
+
+test.testMethod()
+
+print( test.wasRun )
